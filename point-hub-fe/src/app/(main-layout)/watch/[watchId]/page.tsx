7f3d7b2a08f418decaf280eeafe78a680ccdc97a
Nam Nguyen <namgaxilem@gmail.com>
2024-11-14 21:20:46 +0700

    implement detail watch using videojs

--- point-hub-fe/src/app/(main-layout)/watch/[watchId]/page.tsx
+++ point-hub-fe/src/app/(main-layout)/watch/[watchId]/page.tsx
@@ -1,3 +1,5 @@
+import DesktopAd from "./_views/DesktopAd";
+import MobileAd from "./_views/MobileAd";
 import VideoPlayer from "./_views/VideoPlayer";
 import VideoSuggestion from "./_views/VideoSuggestion";
 
@@ -10,8 +12,15 @@ export default async function Page() {
   //   const slug = (await params).watchId;
   return (
     <>
-      <VideoPlayer />
+      <div className="p-[20px] flex gap-3">
+        <VideoPlayer />
+        <DesktopAd />
+      </div>
+      <MobileAd />
+      <br />
       <VideoSuggestion />
+      <br />
+      <br />
     </>
   );
 }
