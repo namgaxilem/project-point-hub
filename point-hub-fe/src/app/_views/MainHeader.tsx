891606910c9e449d6f72d7d70fbe84bc2e4e7c54
Nam Nguyen <namgaxilem@gmail.com>
2024-11-15 22:44:43 +0700

    config multi lang | main logo

--- point-hub-fe/src/app/_views/MainHeader.tsx
+++ point-hub-fe/src/app/_views/MainHeader.tsx
@@ -2,6 +2,8 @@
 
 import { SearchIcon } from "@/icons/SearchIcon";
 import {
+  Accordion,
+  AccordionItem,
   Input,
   Modal,
   ModalBody,
@@ -14,6 +16,8 @@ import {
 import Link from "next/link";
 import ChangeLangBtn from "./ChangeLangBtn";
 import DesktopDarkmode from "./DarkmodeToggle";
+import { Logo } from "./Logo";
+import { useLang } from "@/contexts";
 
 const Categories = () => {
   return (
@@ -123,62 +127,53 @@ const Categories = () => {
 };
 
 const MobileSideMenuContent = () => {
+  const { lang } = useLang();
+
   return (
     <ModalBody className="p-0">
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-        <ul className="space-y-2 font-medium">
+        <ul className="space-y-2 font-medium mb-3">
           <li>
             <Link
               href="/"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              className="flex items-center py-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-6 sm:h-9"
-                alt="Flowbite Logo"
-              />
-              <span className="ms-3">Trang chu</span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            >
-              <svg
-                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-              </svg>
-              <span className="flex-1 ms-3 whitespace-nowrap">Xem nhieu</span>
-              <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                3
+              <span className="flex-1 ms-3 whitespace-nowrap">
+                {lang.header.homePage}
               </span>
             </Link>
           </li>
           <li>
             <Link
-              href="/"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              href="/top-watches"
+              className="flex items-center py-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
-              <svg
-                className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 18"
-              >
-                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-              </svg>
-              <span className="flex-1 ms-3 whitespace-nowrap">The loai</span>
+              <span className="flex-1 ms-3 whitespace-nowrap">
+                {lang.header.topWatch}
+              </span>
             </Link>
           </li>
+          <Accordion isCompact defaultExpandedKeys={["1"]} className="p-0 m-0">
+            <AccordionItem
+              key={"1"}
+              className="p-0 m-0"
+              title={
+                <li>
+                  <Link
+                    href="/"
+                    className="flex items-center py-1 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    <span className="flex-1 ms-3 whitespace-nowrap">
+                      {lang.header.categories}
+                    </span>
+                  </Link>
+                </li>
+              }
+            >
+              <Categories />
+            </AccordionItem>
+          </Accordion>
         </ul>
-        <Categories />
       </div>
     </ModalBody>
   );
@@ -186,6 +181,7 @@ const MobileSideMenuContent = () => {
 
 export default function MainHeader() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { lang } = useLang();
 
   const onOpenMobileMenu = () => {
     onOpen();
@@ -196,14 +192,7 @@ export default function MainHeader() {
       <nav className="bg-bgLight px-4 lg:px-6 py-2.5 dark:bg-bgDark border-b-1 dark:border-dividerDark border-dividerLight">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link href="/" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              JAV
-            </span>
+            <Logo />
           </Link>
           <div
             className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
@@ -216,7 +205,7 @@ export default function MainHeader() {
                   className="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 lg:p-0 dark:text-white"
                   aria-current="page"
                 >
-                  Trang chủ
+                  {lang.header.homePage}
                 </Link>
               </li>
               <li>
@@ -244,7 +233,7 @@ export default function MainHeader() {
                       "!cursor-text",
                     ],
                   }}
-                  placeholder="Type to search..."
+                  placeholder={lang.header.searchHolder}
                   startContent={
                     <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
                   }
@@ -255,13 +244,13 @@ export default function MainHeader() {
                   href="/top-watches"
                   className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Xem nhiều
+                  {lang.header.topWatch}
                 </Link>
               </li>
               <li>
                 <Tooltip className="p-0" content={<Categories />}>
                   <Link href="/categories" className="flex items-center gap-1">
-                    <span>The loai</span>
+                    <span>{lang.header.categories}</span>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -331,7 +320,7 @@ export default function MainHeader() {
         size="full"
       >
         <ModalContent className="md:w-[80vw] w-[85vw] h-full absolute right-0">
-          <ModalHeader>
+          <ModalHeader className="p-2">
             <button onClick={() => onClose()}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -348,12 +337,18 @@ export default function MainHeader() {
                 />
               </svg>
             </button>
+            <Link
+              href="/"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <Logo />
+            </Link>
           </ModalHeader>
           <MobileSideMenuContent />
           <ModalFooter className="flex-col px-2 py-5">
             <Input
               isClearable
-              label={`Tim kiem`}
+              label={lang.header.searchTitle}
               radius="lg"
               classNames={{
                 label: "text-black/50 dark:text-white/90",
@@ -376,7 +371,7 @@ export default function MainHeader() {
                   "!cursor-text",
                 ],
               }}
-              placeholder="Type to search..."
+              placeholder={lang.header.searchHolder}
               startContent={
                 <SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
               }
